58c3397046c13b26a3c8ba19df99eb88eddfe55e
bye0607 <bye0607@gamil.com>
2022-12-01 21:03:34 +0900

    add coment, js code modify

--- js/common.js
+++ js/common.js
@@ -1,7 +1,6 @@
+// visual 영역을 창크기로 확대
 // window 객체(웹브라우저의 창(window))의 width(), height()로 지정
 $(window).resize(function(){
-    // .tubl : tubular 플러그인 적용 요소
-    //         동영상이 풀스크린으로 나온 뒤 .wrapper영역은 스크롤시 보이도록 하기 위해 해당 영역의 크기 조절
     $('.visual_bg').width($(window).width()).height($(window).height());
 });
 
@@ -9,7 +8,7 @@ setTimeout(function(){
     $(window).resize()
 }, 1000);
 
-// 메인 - vegas jquery plugin 배경이미지 슬라이드 효과
+// visual 영역에 vegas jquery plugin 배경이미지 슬라이드 효과
 $(".visual_bg").vegas({
     animation: 'random',
     delay: 5000,
@@ -22,23 +21,21 @@ $(".visual_bg").vegas({
     overlay: '../img/vegas_overlay_04.png'  //배경이미지 위에 dot pattern 이미지 적용
 });
 
-// 메인 - Owl Carousel로 .imgslide 영역에 이미지 슬라이드 효과 적용
+// 홍보센터 영역에 Owl Carousel로이미지 슬라이드 효과 적용
 $('.imgslide').owlCarousel({
     loop:true,
     margin:20,
+    autoplay:true,
     responsiveClass:true,
     responsive:{
         0:{
-            items:1,
-            nav:false
+            items:1
         },
         600:{
-            items:2,
-            nav:false
+            items:2
         },
         1000:{
-            items:4,
-            nav:false
+            items:4
         }
     }
 })
